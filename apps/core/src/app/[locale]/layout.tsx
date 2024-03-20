98efc025963f0456781ff24a44293b48d5589667
Tin Votan <tin.votan@votan-enterprise.com>
2024-03-20 11:45:31 +0100

    fix page load bug

--- apps/core/src/app/[locale]/layout.tsx
+++ apps/core/src/app/[locale]/layout.tsx
@@ -1,7 +1,31 @@
+import CookieBannerComponent from "@/components/cookie-banner-component";
+import { getTranslations } from "next-intl/server";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  params: { locale: string };
+};
+
+export async function generateMetadata({
+  params: { locale },
+}: Omit<Props, "children">) {
+  const t = await getTranslations({ locale, namespace: "signin" });
+
+  return {
+    title: t("title"),
+  };
+}
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default function LocaleLayout({ children }: RootLayoutProps) {
-  return children;
+export default async function PlatformLayout({ children }: RootLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col p-8 sm:p-12">
+      <main className="flex-1">{children}</main>
+      <CookieBannerComponent />
+    </div>
+  );
 }
--- apps/core/src/app/[locale]/page.tsx
+++ apps/core/src/app/[locale]/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from "@shared-components/ui/link";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 
-export default function RootPage() {
-  redirect("/");
+type Props = {
+  params: { locale: string };
+};
+
+export async function generateMetadata({
+  params: { locale },
+}: Omit<Props, "children">) {
+  const t = await getTranslations({ locale, namespace: "verify" });
+
+  return {
+    title: t("title"),
+  };
+}
+
+export default function Page({ params: { locale } }: Props) {
+  // Enable static rendering
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("LocaleSwitcher");
+
+  return <div className="flex-1 space-y-4">{t("description")}</div>;
 }
